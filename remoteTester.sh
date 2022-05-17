c20c3792f019060aefc662e3cdb7653bf0b14eeb
Cole <colescarter@gmail.com>
2022-05-16 22:53:33 -0700

    added a remoteTester.sh file

---
+++ remoteTester.sh
@@ -0,0 +1,2 @@
+/software/CSE/oracle-java-17/jdk-17.0.1/bin/javac -cp .:lib/junit-4.12.2.jar:lib/hamcrest-core-1.3.jar MarkdownParseTest.java
+/software/CSE/oracle-java-17/jdk-17.0.1/bin/java -cp .:lib/junit-4.12.2.jar:lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore MarkdownParseTest
